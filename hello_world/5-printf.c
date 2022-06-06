1d30ee89afddf92ee0c8e667c7092e9a934dc90d
IrlisBuzo <lisi.irlis@gmail.com>
2022-06-05 22:22:59 -0700

    hello world

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -6,9 +6,7 @@
  *     */
 int main(void)
 {
-		char myStr[] = "\"Programming is like building a multilingual puzzle";
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 
-			puts(myStr);
-
-				return (0);
+			return (0);
 }
