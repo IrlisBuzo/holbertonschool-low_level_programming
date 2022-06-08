0e2cb3fad2f24b3e0edf679ceae4a8f094cece86
IrlisBuzo <lisi.irlis@gmail.com>
2022-06-08 13:09:13 -0700

    Nesty loops

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,9 @@
 #include "main.h"
- /**
-  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
-  */
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times.
+ *
+ * Return: Always 0 (Success)
+ */
 void print_alphabet_x10(void)
 {
 	int count = 0;
