8792db9fffe122f8beebb9ae966bceeca178b915
IrlisBuzo <lisi.irlis@gmail.com>
2022-06-08 00:01:35 -0700

    nesty loops

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+
+}
